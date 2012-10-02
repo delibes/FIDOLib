1951f47022462b307f1ea836a7cc7853f411990d
Steen Andersen <sta.anderse@gmail.com>
2012-10-02 17:04:28 +0200

    Testing Git

---
+++ src/data/Constants.java
@@ -0,0 +1,213 @@
+/*
+ * Constants used throughout the program for color schemes, text on buttons etc.
+ */
+package data;
+
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Steen Andersen
+ */
+public class Constants {
+
+    
+     // Colors
+    public static Color backGroundColor = Color.black;
+    public static Color textColor = Color.white;
+    public static Color skyColor = Color.BLUE;
+    
+    public static Color warningColor = Color.red;
+    public static Color progressBarColor = Color.GRAY;
+    public static Color flightStatusNOKcolor = Color.GREEN;
+    public static Color flightStatusConfirmedColor = Color.RED;
+        public static Color ESD139Color = Color.BLUE;
+        public static Color sputnikColor = Color.RED;
+    public static Color hjortoeColor = Color.BLUE;
+    public static Color smaragdColor = Color.GREEN;
+    public static Color otherVesselsColor = Color.YELLOW;
+    public static Color otherVesselsNoMovementColor = Color.WHITE;
+    public static Color baseStationColor = Color.BLUE;
+    public static Color otherVesselsTextColor = Color.BLACK;
+        public static Color knotsLabelColor = Color.BLACK;
+    public static int knotsLabelSize = 16;
+
+    /**
+     * True headning N/A AIS value
+     */
+    public static int trueHeadingNA = 511;
+    /**
+     * Draw the legend on the text panel
+     */
+    public static boolean drawLegend = false;
+    /**
+     * Draw the text values on the text panel
+     */
+    public static boolean drawTextValues = false;
+    /**
+     * Perform a checksum validation on data
+     */
+    public static boolean useCheckSum = false;
+
+    /**
+     * Configuration file name
+     */
+    public static String configurationFile = "configuration.txt";
+    /**
+     *  Update GUI deltaT_GUI every msec.
+     */
+    public static int deltaT_GUI = 100;
+     /**
+     *  Trigger the log timer every msec
+     */
+    public static int triggerLogTimer = 500;
+    /**
+     *  Write to log file every deltaLog msec in case no data has beeing received
+     */
+    public static long deltaLogTimer = 1000;
+    /**
+     * Count down timer delta_T
+     */
+    public static int countDownTimerDelta_T = 1000;
+    // Degree sign
+    public static char degreeChar = 186;
+    // Count down clock start button text
+    public static String startButtonTxt = "Start";
+    // Count down clock hold button text
+    public static String holdButtonTxt = "Hold";
+    // Count down clock restart button text
+    public static String restartButtonTxt = "Restart";
+    // Count down clock stop button text
+    public static String stopButtonTxt = "Stop";
+    // Count down clock reset button text
+    public static String resetButtonTxt = "Reset";
+    // LES tower string
+    public static String LESTowerStr = "LES Tower";
+    // Top domestring
+    public static String topDomeStr = "Top Dome";
+    // 3 ring parachute
+    public static String threeRingParachutesStr = "Chutes Dpl";
+    // 3 ring release tower string
+    public static String threeRingParachutesReleaseStr = "Chutes Rls";
+    // up-righting bags tower string
+    public static String upRightingBagsStr = "Bags";
+     /**
+     * AIS column log separator
+     */
+    public static String AISColumnSeparator = "\t";
+    /**
+     * N/A string
+     */
+    public static String naString = "N/A";
+     /**
+     * Remove vessel which havent been update in removeVesselMinutes (in minutes)
+     */
+    public static long removeVesselMinutes = 1000 * 60 * 60;
+    //public static long removeVesselMinutes = 3000;
+    /**
+     * A nautical mile in meters
+     */
+    public static double nauticalMile = 1852.0;
+    /**
+     * Knots label
+     */
+    public static String knotsLabel = " Kt";
+    /**
+     * Latitude label
+     */
+    public static String latStr = "Lat";
+    /**
+     * Longitude label
+     */
+    public static String lonStr = "Lon";
+    
+    /**
+     * Progress bar indication
+     */
+    public static int progressbarASS = -1;
+    /**
+    /**
+     * Progress bar indication
+     */
+    public static int progressbarSTART = -1;
+    /**
+     * Progress bar indication
+     */
+    public static int progressbarEND = -1;
+    
+   
+    /**
+     * The g force
+     */
+    public static double g = 9.81;
+    /** 
+     * Latitude and longitude convertion factor fom long to double
+     * 
+     */
+    public static double latLonConvertionFactor = 1000000.0;
+    /**
+     * Serial port packet length
+     */
+    public static int packetLength = 13;
+   
+    /**
+     * Begin sim after this period (in 1/10 sec)
+     */
+    public static double sinceLastData = 20;
+    /**
+     * Map corner coordinate
+     */
+    public static double upperLeftCornerLat = 0.0;
+    /**
+     * Map corner coordinate
+     */
+    public static double upperLeftCornerLon = 0.0;
+    /**
+     * Map corner coordinate
+     */
+    public static double lowerRightCornerLat = 0.0;
+    /**
+     * Map corner coordinate
+     */
+    public static double lowerRightCornerLon = 0.0;
+    /**
+     * E139 corner
+     */
+    public static Position E139NorthWest = new Position();
+    /**
+     * E139 corner
+     */
+    public static Position E139NorthEast = new Position();
+    /**
+     * E139 corner
+     */
+    public static Position E139SouthWest = new Position();
+    /**
+     * E139 corner
+     */
+    public static Position E139SouthEast = new Position();
+    /**
+     * Paint ESD139
+     */
+    public static boolean showESD139 = false;
+
+     /**
+     * Degree format
+     */
+    public enum DegreeFormat{DECIMAL, DECIMALMINUTE, DECIMALMINSEC};
+    /**
+     * Actual degree format
+     */
+    public static DegreeFormat degreeFormat = DegreeFormat.DECIMAL;
+    /**
+     * North / South
+     *
+     */
+    public static String northSouth = "N";
+    /**
+     * East / West
+     * 
+     */
+    public static String eastWest = "E";
+}
---
+++ src/data/Position.java
@@ -0,0 +1,185 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import data.Constants;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+/**
+ *
+ * @author Steen Andersen
+ */
+public class Position {
+
+    /**
+     * The latitude in decimal degrees
+     */
+    public double lat = 0.0;
+    /**
+     * The longitude in decimal degrees
+     */
+    public double lon = 0.0;
+    /**
+     * Good latitude reading?
+     */
+    public static boolean latitudeGood = false;
+    /**
+     * Good longitude reading?
+     */
+    public static boolean longitudeGood = false;
+    /**
+     * The latitude in pixels
+     */
+    public int latPixels = 0;
+    /**
+     * The longitude in pixels
+     */
+    public int lonPixels = 0;
+    /**
+     * Decimal symbols
+     */
+    private static DecimalFormatSymbols decimalSymbols = new DecimalFormatSymbols(new Locale("da", "DK"));
+    /**
+     * Formatting  
+     */
+    private DecimalFormat df = new DecimalFormat("", decimalSymbols);
+
+    /**
+     * Constructor
+     */
+    public Position() {
+        decimalSymbols.setDecimalSeparator('.');
+        decimalSymbols.setGroupingSeparator(',');
+
+    }
+
+    /**
+     * Greate circle distance in nautical miles between p1 and p2 using The haversine formula
+     * @param p1 the first point
+     * @param p2 the second point
+     * @return
+     */
+    public static double disanceNauticalMiles(Position p1, Position p2) {
+        if ((p1 == null) || (p2 == null)) {
+            return -1.0;
+        }
+        if ((p1.lat == 0.0) || (p1.lon == 0.0) || (p2.lat == 0.0) || (p2.lat == 0.0)) {
+            return -1.0;
+        }
+        // Use radians
+        double lat1 = p1.lat * Math.PI / 180;
+        double lon1 = p1.lon * Math.PI / 180;
+        double lat2 = p2.lat * Math.PI / 180;
+        double lon2 = p2.lon * Math.PI / 180;
+        double distance = 2 * Math.asin(Math.sqrt(Math.pow((Math.sin((lat1 - lat2) / 2)), 2)
+                + Math.cos(lat1) * Math.cos(lat2) * Math.pow((Math.sin((lon1 - lon2) / 2)), 2))); // Distance in radians
+        distance = distance * 180 * 60 / Math.PI;
+        return distance;
+    }
+
+    /**
+     * Calculate the initial bearing from point p1 to p2 along the greate circle path 
+     */
+    public static int initialBearing(Position p1, Position p2) {
+        if ((p1 == null) || (p2 == null)) {
+            return -1;
+        }
+        if ((p1.lat == 0.0) || (p1.lon == 0.0) || (p2.lat == 0.0) || (p2.lat == 0.0)) {
+            return -1;
+        }
+        // Use radians
+        double lat1 = p1.lat * Math.PI / 180;
+        double lon1 = p1.lon * Math.PI / 180;
+        double lat2 = p2.lat * Math.PI / 180;
+        double lon2 = p2.lon * Math.PI / 180;
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double y = Math.sin(dLon) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2)
+                - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+        double bearing = Math.atan2(y, x) / Math.PI * 180;
+        int brng = (360 - (int) (bearing + 90 + 360) % 360);
+        return (brng == 360) ? 0 : brng;
+    }
+
+    /**
+     * Caluculate the new position given x and y meters from the original
+     * including the MLP heading
+     * @param p the position to be adjusted
+     * @param x the x distance in meters
+     * @param y the y distance in meters
+     */
+    public static void latLonRadialDistance(Position p1, double x, double y) {
+        double lat, lon = 0.0;
+        double lat1 = p1.lat * Math.PI / 180;
+        double lon1 = p1.lon * Math.PI / 180;
+
+        double tc = (Math.atan2(-1.0 * x, y)) % (2.0 * Math.PI); // True course
+        double d = Math.sqrt((x * x) + (y * y)) / Constants.nauticalMile; // distance in nautical miles
+        d = d * Math.PI / (180.0 * 60.0); // Distance in radians
+        lat = Math.asin(Math.sin(lat1) * Math.cos(d) + Math.cos(lat1) * Math.sin(d) * Math.cos(tc));
+        if (Math.cos(lat) == 0) {
+            lon = p1.lon;
+        } else {
+            lon = mod(lon1 - Math.asin(Math.sin(tc) * Math.sin(d) / Math.cos(lat)) + Math.PI, 2 * Math.PI) - Math.PI;
+        }
+        p1.lat = (180 / Math.PI) * lat;
+        p1.lon = (180 / Math.PI) * lon * -1;
+
+    }
+
+    /**
+     * The modules function with the sign following the divisor
+     */
+    public static double mod(double x, double y) {
+        double mod = 0.0;
+        mod = y - x * Math.floor(y / x);
+
+        return mod;
+    }
+
+    /**
+     * Format degrees
+     */
+    public static String formatDegrees(double degree) {
+
+        if (Constants.degreeFormat == Constants.DegreeFormat.DECIMAL) {
+            DecimalFormat df = new DecimalFormat("00.00000", decimalSymbols);
+            return df.format(degree) + Constants.degreeChar;
+        } else if (Constants.degreeFormat == Constants.DegreeFormat.DECIMALMINUTE) {
+            int d = (int) degree;
+            double m = (degree - d) * 60;
+            DecimalFormat df = new DecimalFormat("00.000", decimalSymbols);
+            return "" + d + Constants.degreeChar + " " + df.format(m) + "'";
+        } else if (Constants.degreeFormat == Constants.DegreeFormat.DECIMALMINSEC) {
+            int d = (int) degree;
+            double m = (degree - d) * 60;
+            double s = (m - Math.floor(m)) * 60;
+            DecimalFormat dfmin = new DecimalFormat("00", decimalSymbols);
+            DecimalFormat df = new DecimalFormat("00.00", decimalSymbols);
+
+            return "" + d + Constants.degreeChar + " " + dfmin.format(m) + "' " + df.format(s) + "\"";
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Return latitude as string according to the format
+     */
+    public String getLat() {
+        return "N " + formatDegrees(lat);
+    }
+
+    /**
+     * Return longitude as string according to the format
+     */
+    public String getLon() {
+        return "E " + formatDegrees(lon);
+    }
+}
